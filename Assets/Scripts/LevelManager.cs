0eaf198144966200da276d73d6604fcab829c2ad
JCVader <dawidpszenicznykrk@gmail.com>
2018-01-11 21:37:52 +0100

    First Upload
    Splash screen + głowne menu / skrypt na SoundController, LevelManager / 2 fonty i pliki audio.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    float time = 8.3f;
+    
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Invoke("LoadNextLevel", time);
+        }
+    }
+    public void LoadLevel(string level)
+    {
+        Debug.Log("Level loaded: " + level);
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] audioClip;
+    private AudioSource audioSource;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        AudioClip currentClip = audioClip[scene.buildIndex];
+
+        
+        if(currentClip)
+        {
+            audioSource.clip = currentClip;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+
+}
